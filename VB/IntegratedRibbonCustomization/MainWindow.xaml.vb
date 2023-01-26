c73b421375ed80aa310bef7395682d772031e9fa
DevExpressExampleBot <fakeEmail>
2023-01-26 23:03:28 +0400

    Source auto update [skip ci]

--- VB/IntegratedRibbonCustomization/MainWindow.xaml.vb
+++ VB/IntegratedRibbonCustomization/MainWindow.xaml.vb
@@ -1,20 +1,20 @@
-﻿Imports DevExpress.Xpf.Core
-Imports System
+Imports DevExpress.Xpf.Core
 Imports System.Windows
 
 Namespace IntegratedRibbonCustomization
-	''' <summary>
-	''' Interaction logic for MainWindow.xaml
-	''' </summary>
-	Partial Public Class MainWindow
-		Inherits Window
 
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    ''' <summary>
+    ''' Interaction logic for MainWindow.xaml
+    ''' </summary>
+    Public Partial Class MainWindow
+        Inherits Window
 
-		Private Sub About_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
-			DXMessageBox.Show("Click!", "Integrated Ribbon Customization", MessageBoxButton.OK, MessageBoxImage.Information)
-		End Sub
-	End Class
+        Public Sub New()
+            Me.InitializeComponent()
+        End Sub
+
+        Private Sub About_ItemClick(ByVal sender As Object, ByVal e As DevExpress.Xpf.Bars.ItemClickEventArgs)
+            DXMessageBox.Show("Click!", "Integrated Ribbon Customization", MessageBoxButton.OK, MessageBoxImage.Information)
+        End Sub
+    End Class
 End Namespace
--- VB/IntegratedRibbonCustomization/SourceHelper.vb
+++ VB/IntegratedRibbonCustomization/SourceHelper.vb
@@ -1,15 +1,11 @@
-﻿Imports System
-Imports System.Collections.Generic
-Imports System.Linq
-Imports System.Text
-Imports System.Threading.Tasks
-
 Namespace IntegratedRibbonCustomization
-	#Region "#SourceHelper"
-	Public Module SourceHelper
-		Public Function GetDocumentSource() As String
-			Return "Grimm.docx"
-		End Function
-	End Module
-	#End Region ' #SourceHelper
+
+#Region "#SourceHelper"
+    Public Module SourceHelper
+
+        Public Function GetDocumentSource() As String
+            Return "Grimm.docx"
+        End Function
+    End Module
+#End Region  ' #SourceHelper
 End Namespace
